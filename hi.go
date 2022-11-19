8fe87ae5bbb9d215c9b5f3b4c55cb06d082eb966
Akilan <akil.dove@gmail.com>
2022-11-19 20:54:05 +0530

    updated notes

--- hi.go
+++ hi.go
@@ -2,8 +2,10 @@ package main
 
 import "fmt"
 
+// it is consumed by main.go
 var myName = "Akilan"
 
+// it is consumed by main.go
 func sayHi() {
 	fmt.Println("Hi Akilan")
 }
--- main.go
+++ main.go
@@ -9,15 +9,16 @@ import (
 
 func main() {
 
+	// sayHi() , myName are directly consumed here
 	sayHi()
 	fmt.Println(myName)
 
+	// SayBye() exported because it follows camelcase pattern
 	goodbye.SayBye()
 	// Throws error as byeMessage variable not exported
 	// fmt.Println(goodbye.byeMessage)
 
+	// SayHello() exported because it follows camelcase pattern
 	hello.SayHello()
 
-	//sayBye()
-	//sayHello()
 }
